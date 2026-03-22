8c5e2e23a0296d7ad572ac173d7151d22be4fd5c
KRISH CHAUDHARY <krishchaudhary144@gmail.com>
2026-03-22 18:08:29 +0530

    Update hash_password.py

--- hash_password.py
+++ hash_password.py
@@ -3,7 +3,6 @@ from app.core.security import hash_password
 plain_password = "krish@8252"
 
 hashed = hash_password(plain_password)
-
 print("Plain Password:", plain_password)
 print("Hashed Password:")
 print(hashed)
